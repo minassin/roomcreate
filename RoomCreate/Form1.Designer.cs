b18bc0cb0e75d10198dfe3964db4aa5c02050351
minassin <134121274+minassin@users.noreply.github.com>
2023-05-22 13:15:40 +0800

    Add files via upload

---
+++ RoomCreate/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace RoomCreate
+{
+    partial class RoomCreate
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_roomID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt_roomNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_200 = new System.Windows.Forms.Button();
+            this.btn_100 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txt_roomID);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(341, 60);
+            this.panel1.TabIndex = 0;
+            // 
+            // txt_roomID
+            // 
+            this.txt_roomID.Location = new System.Drawing.Point(108, 21);
+            this.txt_roomID.Name = "txt_roomID";
+            this.txt_roomID.Size = new System.Drawing.Size(176, 25);
+            this.txt_roomID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "房间名：";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txt_roomNumber);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 60);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(341, 60);
+            this.panel2.TabIndex = 1;
+            // 
+            // txt_roomNumber
+            // 
+            this.txt_roomNumber.Location = new System.Drawing.Point(108, 22);
+            this.txt_roomNumber.Name = "txt_roomNumber";
+            this.txt_roomNumber.Size = new System.Drawing.Size(176, 25);
+            this.txt_roomNumber.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "房间号：";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btn_200);
+            this.panel3.Controls.Add(this.btn_100);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 120);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(341, 60);
+            this.panel3.TabIndex = 2;
+            // 
+            // btn_200
+            // 
+            this.btn_200.Location = new System.Drawing.Point(209, 15);
+            this.btn_200.Name = "btn_200";
+            this.btn_200.Size = new System.Drawing.Size(75, 23);
+            this.btn_200.TabIndex = 3;
+            this.btn_200.Text = "200";
+            this.btn_200.UseVisualStyleBackColor = true;
+            // 
+            // btn_100
+            // 
+            this.btn_100.Location = new System.Drawing.Point(108, 15);
+            this.btn_100.Name = "btn_100";
+            this.btn_100.Size = new System.Drawing.Size(75, 23);
+            this.btn_100.TabIndex = 2;
+            this.btn_100.Text = "100";
+            this.btn_100.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "人数：";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.txt_Password);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 180);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(341, 60);
+            this.panel4.TabIndex = 3;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(108, 15);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.PasswordChar = '*';
+            this.txt_Password.Size = new System.Drawing.Size(176, 25);
+            this.txt_Password.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "密码：";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btn_create);
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 240);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(341, 60);
+            this.panel5.TabIndex = 4;
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(153, 19);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(75, 23);
+            this.btn_create.TabIndex = 3;
+            this.btn_create.Text = "创建";
+            this.btn_create.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "创建：";
+            // 
+            // RoomCreate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 311);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "RoomCreate";
+            this.Text = "创建房间";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox txt_roomID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_roomNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_200;
+        private System.Windows.Forms.Button btn_100;
+        private System.Windows.Forms.Button btn_create;
+    }
+}
+
